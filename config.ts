4a5975e1f4fe548e09ad6536f0c8542e8fe6d842
arvi-ey <tuhinroyand@gmail.com>
2025-01-26 09:19:51 +0530

    config changes

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-// export const URL = import.meta.env.VITE_URL
-export const URL = "http://localhost:5500/"
+export const URL = import.meta.env.VITE_URL
+// export const URL = "http://localhost:5500/"
